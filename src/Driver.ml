3e354952d7d0a77b5e93458a8b0dfbbebb26796f
dboulytchev <dboulytchev@gmail.com>
2016-09-09 15:36:28 +0300

    Run

--- src/Driver.ml
+++ src/Driver.ml
@@ -1,1 +1,24 @@
-let _ = ()
+open Expr
+
+(*
+read (x);  
+  read (y);
+    z := x + y;
+    write (z*z)
+*)
+
+let p =
+  Seq (
+      Read "x",
+      Seq (
+          Read "y",
+          Seq (
+              Assign ("z", Add (Var "x", Var "y")),
+              Write (Mul (Var "z", Var "z"))
+          )
+      )
+  )
+
+let _ =
+  let [r] = run [4; 5] p in
+  Printf.printf "%d\n" r
--- src/Expr.ml
+++ src/Expr.ml
@@ -1,1 +1,36 @@
-let _ = ()
+type expr =
+  | Const of int
+  | Var   of string
+  | Add   of expr * expr
+  | Mul   of expr * expr
+
+let rec eval (state : string -> int) expr =
+  match expr with
+  | Const n     -> n
+  | Var   x     -> state x
+  | Add  (l, r) -> eval state l + eval state r
+  | Mul  (l, r) -> eval state l * eval state r
+  
+type stmt =
+  | Skip
+  | Read   of string
+  | Write  of expr
+  | Assign of string * expr
+  | Seq    of stmt * stmt
+
+let run (input : int list) stmt =
+  let rec run' ((state, input, output) as c) stmt =
+    let state' x = List.assoc x state in
+    match stmt with
+    | Skip -> c
+    | Seq (l, r) -> run' (run' c l) r
+    | Read x ->
+       let y::input' = input in
+       ((x, y)::state, input', output)
+    | Assign (x, e) ->
+       ((x, eval state' e)::state, input, output)
+    | Write e ->
+       (state, input, output @ [eval state' e])
+  in
+  let (_, _, output) = run' ([], input, []) stmt in
+  output
